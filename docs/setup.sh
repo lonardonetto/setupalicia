5af0aae45cb308ce131370864258786e005e1c03
Leonardo Netto <webvendedor@yahoo.com.br>
2025-09-22 07:43:11 -0300

    Update setup.sh

--- docs/setup.sh
+++ docs/setup.sh
@@ -3,5 +3,5 @@ set -Eeuo pipefail
 
 RAW="https://raw.githubusercontent.com/lonardonetto/setupalicia/main/instalacao_corrigida.sh"
 
-# Baixa a versão oficial do instalador e repassa todos os argumentos
-curl -fsSL "$RAW" | bash -s -- "$@"
+# Preserva o TTY para o menu interativo
+bash <(curl -fsSL "$RAW") -- "$@"
